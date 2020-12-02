37d89d4dc183ad2c6fa274ddc6cdfc9ecf906eb6
Jeffrey Wang <jwang378@jhu.edu>
2020-12-02 14:44:15 -0500

    Tried readin, not sure if this is what it's supposed to be yet but for now it takes a string stream of ints and makes them into cards, which it pushes into a vector

--- Pile.h
+++ Pile.h
@@ -18,7 +18,16 @@ class Pile {
 
   std::string toString() const;  // for saving state
 
-  void readIn(std::istream & is);  // students to write this, companion to toString()
+  void readIn(std::istream & is){
+
+    while(!is.eof()){
+      int n;
+      is >> n;
+      Card c = new Card(n);
+      pile.push_back(c);
+    }
+    
+  }// students to write this, companion to toString()
 
   virtual void display() const = 0;  // for live game play, must override
 };
