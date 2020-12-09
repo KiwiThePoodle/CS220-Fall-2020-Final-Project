5d16cc8eff3a438a57c68071a76707c09cda5eb2
Thomas-Yu <42897266+Thomas-Yu@users.noreply.github.com>
2020-12-08 22:34:09 -0800

    Update SkipBoGame.cpp

--- SkipBoGame.cpp
+++ SkipBoGame.cpp
@@ -149,7 +149,7 @@ SkipBoGame::SkipBoGame(bool isS, std::string file){
       int cardNum;
       save >> cardNum;
       Card* c = new Card(cardNum);
-      sp->addCard(c);
+      sp->addCard(*c);
     }
 
     save >> tempString; //gets rid of "Hand"
