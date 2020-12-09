4b017038c4c7b0dcf3cb5fd35a081be249114e42
Thomas-Yu <42897266+Thomas-Yu@users.noreply.github.com>
2020-12-08 22:13:13 -0800

    Update SkipBoGame.cpp

--- SkipBoGame.cpp
+++ SkipBoGame.cpp
@@ -111,7 +111,8 @@ SkipBoGame::SkipBoGame(bool isS, int pCount, int stk, std::string file) {
 
   int index = curp;
   for(int i = 0; i < nump; i++) {
-    peep[index]->setStockPile(stocks[i]);
+    FaceUpPile temp = stocks.at(i);
+    peep[index]->setStockPile(temp);
     index = (index + 1) % nump;
   }
   
