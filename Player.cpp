ca09e3770b3096b8b7840ebc8c1945a588c27353
Thomas-Yu <42897266+Thomas-Yu@users.noreply.github.com>
2020-12-08 21:03:27 -0800

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -59,13 +59,13 @@ void Player::readIn(std::istream & is) {
   is >> size;   //first int found is stock pile size
   for(int i = 0; i < size; i++) {   //loop through stock size number of times
     is >> cardNumber;   //each int is a number representing a card
-    stock.addCard(Card(cardNumber));   //add new cards to stock pile for each number
+    stock->addCard(Card(cardNumber));   //add new cards to stock pile for each number
   }
   is >> temp;
   is >> size;   //next int is hand pile size
   for(int i = 0; i < size; i++) {   //adds hand size number of cards to hand pile
     is >> cardNumber;
-    handPile.addCard(Card(cardNumber));
+    handPile->addCard(Card(cardNumber));
   }
   is >> temp;
   is >> size;   //next int is discard[0] pile size
@@ -104,21 +104,21 @@ Player::~Player() {   //deletes each pile for a player
 }
 
 void Player::drawToHand() {                   //called at the start of player's turn, draws cards until hand is full
-  int cardsToDraw = 5 - handPile.size();
+  int cardsToDraw = 5 - handPile->size();
   for(int i = 0; i < cardsToDraw; i++) {
-    handPile.addCard(updatedDrawPile.topCard());   //adds top card from draw pile to hand, *removes card from draw pile*
+    handPile->addCard(updatedDrawPile->topCard());   //adds top card from draw pile to hand, *removes card from draw pile*
   }
 }
 
 void Player::handToDiscard(int handNum, int discardNum) {   //moves card from hand to a discard pile
   handNum -= 5;
-  Card temp = handPile.removeCard(handNum);
+  Card temp = handPile->removeCard(handNum);
   discardNum -= 1;
   discard[discardNum].addCard(temp);
 }
 
 Card Player::getStockPileCard() {   //returns top card of stock
-  return stock.topCard();
+  return stock->topCard();
 }
 
 Card Player::getDiscardPileCard(int discardNum) {   //returns top card of a discard pile
@@ -128,6 +128,6 @@ Card Player::getDiscardPileCard(int discardNum) {   //returns top card of a disc
 
 Card Player::getHandPileCard(int handNum) {   //returns a card from hand
   handNum -= 5;
-  Card temp = handPile.removeCard(handNum);
+  Card temp = handPile->removeCard(handNum);
   return temp;
 }
