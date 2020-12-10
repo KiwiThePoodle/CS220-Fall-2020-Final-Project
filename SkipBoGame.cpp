8e89ab787be0e93fa1739e4b3faa2ffa39fe0941
jwang378 <70606745+jwang378@users.noreply.github.com>
2020-12-09 19:45:36 -0500

    Update SkipBoGame.cpp

--- SkipBoGame.cpp
+++ SkipBoGame.cpp
@@ -301,7 +301,7 @@ bool SkipBoGame::play(int p){
     if (f == 0){
       c = peep[p]->getStockPileCard();
     }else if (f >= 1 && f <= 4){
-      c = peep[p]->getDiscardPileCard(f-1);
+      c = peep[p]->getDiscardPileCard(f);
     }else if (f >= 5 && f <= 9){
       c = peep[p]->getHandPileCard(f);
     }
