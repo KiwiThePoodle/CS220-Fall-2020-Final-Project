58128b157ab0f604a884e12b9cc6f52e51b0ba55
KiwiThePoodle <42877043+KiwiThePoodle@users.noreply.github.com>
2020-12-09 17:34:15 -0800

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -111,9 +111,6 @@ void Player::drawToHand() {                   //called at the start of player's 
 }
 
 void Player::handToDiscard(Card c, int discardNum) {   //moves card from hand to a discard pile
-  //handNum -= 5;
-  //Card temp = handPile->removeCard(handNum);
-  //handPile->addCard(temp);
   discardNum -= 1;
   discard[discardNum].addCard(c);
 }
