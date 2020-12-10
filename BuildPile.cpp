d2b9db343f247bb058a8c1712708997a553ff9fc
Thomas-Yu <42897266+Thomas-Yu@users.noreply.github.com>
2020-12-09 23:57:10 -0800

    Update BuildPile.cpp

--- BuildPile.cpp
+++ BuildPile.cpp
@@ -4,7 +4,7 @@
 void BuildPile::addCards(DrawPile* d) {
 
   for(int i = 0; i < (int)pile.size(); i++) {
-    Card c = this.topCard();
+    Card c = this->topCard();
     d->addCard(c);
   }
   cardNumber = 0;
