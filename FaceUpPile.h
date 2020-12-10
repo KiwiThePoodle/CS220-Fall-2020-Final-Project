ff2fe5443b36aeb91f6c1673d50bae9f33c19f06
KiwiThePoodle <42877043+KiwiThePoodle@users.noreply.github.com>
2020-12-09 17:16:49 -0800

    Update FaceUpPile.h

--- FaceUpPile.h
+++ FaceUpPile.h
@@ -6,24 +6,14 @@
 #include "Pile.h"
 
 class FaceUpPile : public Pile{
- //protected:
-  //Pile pile;
  public:
   FaceUpPile() { pile = std::vector<Card>{};}
-  // pile = std::vector<Card>;
-  
 
   FaceUpPile(std::vector<Card> f) { pile = f;}
 
   void display() const;
 
-  //~FaceUpPile();
-
-  //virtual void addCard(const Card& c) { pile.push_back(c); }
-
-  //Card topCard();
-
-  //int size() { return pile.size(); }
+  //other needed functions inherited from pile.h
 };
 
 #endif
