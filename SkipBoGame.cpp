f3084309b73bd4275083ecd7607dd08b052f050b
jwang378 <70606745+jwang378@users.noreply.github.com>
2020-12-09 22:07:19 -0500

    Update SkipBoGame.cpp

--- SkipBoGame.cpp
+++ SkipBoGame.cpp
@@ -239,6 +239,15 @@ bool SkipBoGame::checkWin(){
   return false;
 }
 
+void SkipBoGame::illegalMove(){
+    std::cout << "illegal command, try again" << std::endl << std::endl;
+    display();
+    std::cout << "(m)ove [start] [end] or (d)raw ? ";
+    std::cin >> action;
+    std::cin >> from;
+    std::cin>>to;
+}
+
 bool SkipBoGame::play(int p){
   std::cout << "(m)ove [start] [end] or (d)raw ? ";
   std::string action;
@@ -249,12 +258,7 @@ bool SkipBoGame::play(int p){
   std::cin >> from;
   std::cin>>to;   //depending on user input (movve) checks validity
   while ((from == "0" && (to == "1" || to == "2" || to == "3" || to == "4")) || (action!= "m" && action != "d") || (from != "0" && from != "1" && from != "2" && from != "3" && from != "4" && from != "5" && from != "6" && from != "7" && from != "8" && from != "9")  ||(to != "a" && to != "b" && to != "c" && to != "d" && to != "1" && to != "2" && to != "3" && to != "4" )){
-    std::cout << "illegal command, try again" << std::endl << std::endl;
-    display();
-    std::cout << "(m)ove [start] [end] or (d)raw ? ";
-    std::cin >> action;
-    std::cin >> from;
-    std::cin>>to;
+    illegalMove();
   }
 	
   std::cout << std::endl;
