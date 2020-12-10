3379a722744a275352fe3c616a17fa8a322a8463
Thomas-Yu <42897266+Thomas-Yu@users.noreply.github.com>
2020-12-09 19:13:47 -0800

    Update SkipBoGame.cpp

--- SkipBoGame.cpp
+++ SkipBoGame.cpp
@@ -260,7 +260,7 @@ bool SkipBoGame::play(int p){
 
   std::cin >> action;
   std::cin >> from;
-  std::cin>>to;   //depending on user input (movve) checks validity
+  std::cin>>to;   //depending on user input (move) checks validity
   while ((from == "0" && (to == "1" || to == "2" || to == "3" || to == "4")) || (action!= "m" && action != "d") || (from != "0" && from != "1" && from != "2" && from != "3" && from != "4" && from != "5" && from != "6" && from != "7" && from != "8" && from != "9")  ||(to != "a" && to != "b" && to != "c" && to != "d" && to != "1" && to != "2" && to != "3" && to != "4" )){
     std::stringstream ss = illegalMove();
 	  action << ss;
