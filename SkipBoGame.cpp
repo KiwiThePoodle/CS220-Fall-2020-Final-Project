a7484e7cdeecbba4a2d4a8c550fab2d6a3214921
jwang378 <70606745+jwang378@users.noreply.github.com>
2020-12-09 20:23:28 -0500

    Update SkipBoGame.cpp

--- SkipBoGame.cpp
+++ SkipBoGame.cpp
@@ -266,7 +266,7 @@ bool SkipBoGame::play(int p){
   std::cin >> from;
   std::cin>>to;
   while (action!= "m" || action != "d" || from != "0" || from != "1" || from != "2" || from != "3" || from != "4" || from != "5" || from != "6" || from != "7" || from != "8" || from != "9" || to != "a" || to != "b" || to != "c" || to != "d" || to != "1" || to != "2" || to != "3" || to != "4" ){
-  	std::cout << "illegal command, try again" << endl;
+  	std::cout << "illegal command, try again" << std::endl;
   	display();
 	std::cout << "(m)ove [start] [end] or (d)raw ? ";
 	std cin >> action;
