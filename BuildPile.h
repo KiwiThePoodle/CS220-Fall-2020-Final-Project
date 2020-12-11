d641b66923cb8cfb50b0362279ac6af0e9aecc58
KiwiThePoodle <42877043+KiwiThePoodle@users.noreply.github.com>
2020-12-11 15:35:55 -0800

    Update BuildPile.h

--- BuildPile.h
+++ BuildPile.h
@@ -9,17 +9,17 @@
 
 class BuildPile : public FaceUpPile {
  private:
-  int cardNumber;
+  int cardNumber;   //helps keep track of cardNumber in the build pile, even with a wildcard
  public:
   BuildPile() { pile = std::vector<Card>{}; cardNumber = 0; }
 
   BuildPile( std::vector<Card> f) { pile = f; cardNumber = 0; }
-
-  void incrementCardNumber() { cardNumber++; }
+ 
+  void incrementCardNumber() { cardNumber++; }   //called after card is added to build pile
   
-  int getCardNumber() { return cardNumber; }
+  int getCardNumber() { return cardNumber; }   //called to get the current number
 
-  void addCards(DrawPile* d);
+  void addCards(DrawPile* d);   //add cards to draw pile after it becomes empty
 };
 
 #endif
