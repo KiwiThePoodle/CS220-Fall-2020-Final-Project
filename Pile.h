314046d77551764410d516c4066edecdeeedbf2a
Thomas-Yu <42897266+Thomas-Yu@users.noreply.github.com>
2020-12-11 02:56:19 -0800

    Update Pile.h

--- Pile.h
+++ Pile.h
@@ -17,7 +17,7 @@ class Pile {
  
   Pile(std::vector<Card> p) { pile = p; }
  
-  virtual ~Pile(){ }
+  //virtual ~Pile(){ }
  
  std::vector<Card> getPile() {   //returns the pile
   std::vector<Card>r;
